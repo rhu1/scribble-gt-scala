e2630e7a167c28c501e1d35b6ef091a438593411
Laura Voinea <laura.voinea@glasgow.ac.uk>
2026-01-09 10:55:33 +0000

    wip

--- src/main/scala/com/github/rhu1/gt/codegen/CodegenCLI.scala
+++ src/main/scala/com/github/rhu1/gt/codegen/CodegenCLI.scala
@@ -22,7 +22,8 @@ object CodegenCLI {
       scribblePath: String = "",
       protoFilter: Option[String] = None,
       roles: Seq[String] = Seq.empty,
-      outBase: File = new File("./generated")
+      outBase: File = new File("./generated"),
+      emitGC: Boolean = false
   )
 
   def main(argv: Array[String]): Unit = {
@@ -71,15 +72,15 @@ object CodegenCLI {
         } else efsms.keys.toSeq
 
       val allRoles = gtype.getLiveRoles
-      println(s"[Codegen] Generating for protocol $simpleName: roles=${targetRoles.mkString(", ")}, outDir=${outDir.getPath}")
+      println(s"[Codegen] Generating for protocol $simpleName: roles=${targetRoles.mkString(", ")}, outDir=${outDir.getPath}, gc=${args.emitGC}")
 
       targetRoles.foreach { r =>
         val roleAtom = r.toString.toLowerCase
         val efsm = efsms(r)
         val rolesLower = allRoles.toSeq.map(_.toString.toLowerCase)
         writeHrl(outDir.toPath, roleAtom, rolesLower)
-        RuntimeModuleGenerator.generate(simpleName, roleAtom, efsm, outDir)
-        CallbackModuleGenerator.generateCallback(simpleName, roleAtom, efsm, outDir, rolesLower)
+        RuntimeModuleGenerator.generate(simpleName, roleAtom, efsm, outDir, emitGC = args.emitGC)
+        CallbackModuleGenerator.generateCallback(simpleName, roleAtom, efsm, outDir, rolesLower, emitGC = args.emitGC)
         println(s"[Codegen] Wrote gen_${roleAtom}.erl and ${roleAtom}.erl")
       }
     }
@@ -103,6 +104,7 @@ object CodegenCLI {
       val (roleNames, remaining) = tail.span(s => !s.startsWith("-"))
       parseFlags(acc.copy(roles = roleNames), remaining)
     case "-out" :: dir :: tail => parseFlags(acc.copy(outBase = new File(dir)), tail)
+    case "-gc" :: tail => parseFlags(acc.copy(emitGC = true), tail)
     case other :: _ =>
       usage()
       sys.error(s"Unknown flag: $other")
@@ -132,7 +134,14 @@ object CodegenCLI {
     println(
       s"""
          |Usage:
-         |  runMain com.github.rhu1.gt.codegen.CodegenCLI <path/to/file.scr> [-proto Name] (-all | -roles R1 R2 ...) [-out ./generated]
+         |  runMain com.github.rhu1.gt.codegen.CodegenCLI <path/to/file.scr> [-proto Name] (-all | -roles R1 R2 ...) [-out ./generated] [-gc]
+         |
+         |Flags:
+         |  -proto Name   Generate only for the named protocol inside the .scr
+         |  -all          Generate for all roles (default if -roles is omitted)
+         |  -roles ...    Generate only for the listed roles (space-separated)
+         |  -out DIR      Output base directory (default: ./generated)
+         |  -gc           Enable idle GC support (gc_timeout/0, on_gc/2 hooks and runtime scheduling)
          |""".stripMargin)
   }
 }
